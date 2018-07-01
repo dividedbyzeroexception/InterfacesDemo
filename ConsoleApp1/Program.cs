8cb53302d2848440670b01827c8490f6687f6288
dividedbyzero <dividedbyzero@DBZ>
2018-07-01 12:05:58 +0200

    Test comment implemented Throwaway

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    interface IItem {
+            string Name { get; set; }
+            int GoldValue { get; set; }
+
+            void Equip();
+            void Sell();
+    }
+
+    class Weapon : IItem
+    {
+        public string Name { get; set; }
+        public int GoldValue { get; set; }
+
+        public void Equip()
+        {
+            Console.Write("Equipped");
+        }
+
+        public void Sell()
+        {
+            Console.WriteLine("Sold");
+        }
+    }
+
+
+    class Program
+    {
+        
+
+         
+
+        static void Main(string[] args)
+        {
+            List<Weapon> weapons = new List<Weapon>();
+            weapons.Add(new Weapon { Name = "Axe", GoldValue = 100 });
+            weapons.Add(new Weapon { Name = "Hammer", GoldValue = 30 });
+            weapons.Add(new Weapon { Name = "Spear", GoldValue = 50 });
+
+
+
+            string strJson = JsonConvert.SerializeObject(weapons);
+            Console.ReadLine();
+        }
+    }
+}
